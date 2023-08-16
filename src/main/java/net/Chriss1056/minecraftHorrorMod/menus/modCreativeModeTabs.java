3e33f82daa32610a791fcb56f7f2708c65a31967
Chriss1056 <97809719+Chriss1056@users.noreply.github.com>
2023-08-17 01:24:27 +0200

    added second test tab

--- src/main/java/net/Chriss1056/minecraftHorrorMod/menus/modCreativeModeTabs.java
+++ src/main/java/net/Chriss1056/minecraftHorrorMod/menus/modCreativeModeTabs.java
@@ -16,9 +16,21 @@ public class modCreativeModeTabs
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, minecraftHorrorMod.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("test_tab_1",
+    public static final RegistryObject<CreativeModeTab> TEST_TAB_1 = CREATIVE_MODE_TABS.register("test_tab_1",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(modItems.TEST_ITEM_1.get()))
                     .title(Component.translatable("creativeTab.test_tab_1"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(modItems.TEST_ITEM_1.get());
+                        pOutput.accept(modItems.TEST_ITEM_2.get());
+
+                        pOutput.accept(modBlocks.TEST_BLOCK_1.get());
+                        pOutput.accept(modBlocks.TEST_BLOCK_2.get());
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> TEST_TAB_2 = CREATIVE_MODE_TABS.register("test_tab_2",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(modItems.TEST_ITEM_1.get()))
+                    .title(Component.translatable("creativeTab.test_tab_2"))
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(modItems.TEST_ITEM_1.get());
                         pOutput.accept(modItems.TEST_ITEM_2.get());
