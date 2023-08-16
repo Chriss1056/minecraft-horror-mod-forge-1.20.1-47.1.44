d7580e49ba10596d974371befe96475c53ac8145
Chriss1056 <97809719+Chriss1056@users.noreply.github.com>
2023-08-17 01:06:12 +0200

    moved modCreativeModeTabs.java file into its own package called menus

--- src/main/java/net/Chriss1056/minecraftHorrorMod/menus/modCreativeModeTabs.java
+++ src/main/java/net/Chriss1056/minecraftHorrorMod/menus/modCreativeModeTabs.java
@@ -1,5 +1,7 @@
-package net.Chriss1056.minecraftHorrorMod.items;
+package net.Chriss1056.minecraftHorrorMod.menus;
 
+import net.Chriss1056.minecraftHorrorMod.blocks.modBlocks;
+import net.Chriss1056.minecraftHorrorMod.items.modItems;
 import net.Chriss1056.minecraftHorrorMod.minecraftHorrorMod;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -14,12 +16,14 @@ public class modCreativeModeTabs
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, minecraftHorrorMod.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("test_tab",
+    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("test_tab_1",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(modItems.TEST_ITEM_1.get()))
-                    .title(Component.translatable("creativeTab.test_tab"))
+                    .title(Component.translatable("creativeTab.test_tab_1"))
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(modItems.TEST_ITEM_1.get());
                         pOutput.accept(modItems.TEST_ITEM_2.get());
+
+                        pOutput.accept(modBlocks.TEST_BLOCK_1.get());
                     })
                     .build());
 
